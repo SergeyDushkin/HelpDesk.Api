b693cb50d432a6be3fce1bfefc67bd6773bbe55f
ilabudev <ilabudev@yandex.ru>
2017-02-16 00:00:01 +0300

    Промежуточный commit

--- Services/SettingsService.cs
+++ Services/SettingsService.cs
@@ -8,6 +8,8 @@ using System.Net.Http.Headers;
 using Newtonsoft.Json;
 using Microsoft.Extensions.Options;
 using System.Linq;
+using Consul;
+using System.Text;
 
 namespace servicedesk.api
 {
@@ -15,11 +17,13 @@ namespace servicedesk.api
     {
         protected ILogger _logger { get; }
         private readonly HttpClient _client;
+        private ConsulClientConfiguration _consuleconfiguration;
+
         public SettingsService(IOptions<SettingsConfiguration> configuration, ILoggerFactory loggerFactory)
         {
            _logger = loggerFactory.CreateLogger(GetType().Namespace);
-           //_logger.LogInformation (configuration.Value.Url);
            
+           /* 
            _client = new HttpClient(new HttpClientHandler
             {
                 UseProxy = false
@@ -29,87 +33,109 @@ namespace servicedesk.api
             _client.DefaultRequestHeaders.Accept.Clear();
             _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-        }
+            */
 
-/*
-        public async Task<IQueryable<Job>> GetAsync(Guid ticketId)
+            _consuleconfiguration = new ConsulClientConfiguration {
+                Address = new Uri(configuration.Value.Url)
+        };
+    }
+
+        /*
+                public async Task<IQueryable<Job>> GetAsync(Guid ticketId)
+                {
+                    await Task.FromResult(0);
+
+                    this.logger.LogInformation("Get jobs");
+
+                    return this.context.RequestJobs
+                        .Where(r => r.REQUEST_GUID == ticketId)
+                        .Select(r => new Job {
+                            Id = r.GUID_RECORD,
+                            JobNumber = r.JOB_NUMBER,
+                            SupplierId = r.SUPPLIER_GUID.Value,
+                            UserId = r.USER_GUID,
+                            Description = r.COMMENT,
+                            StartDate = r.STARTDATE,
+                            CompleteDate = r.ENDDATE,
+                            Client = new Client { Id = r.SUPPLIER_GUID.Value, Name = r.SUPPLIER.LOCATION_NAME },
+                            User = r.USER != null ? new User { Id = r.USER_GUID.Value, Name = r.USER.FIRST_NAME } : default(User)
+                        });
+                }
+        */
+        public async Task<string> GetByCodeAsync(string code)
         {
-            await Task.FromResult(0);
 
-            this.logger.LogInformation("Get jobs");
-
-            return this.context.RequestJobs
-                .Where(r => r.REQUEST_GUID == ticketId)
-                .Select(r => new Job {
-                    Id = r.GUID_RECORD,
-                    JobNumber = r.JOB_NUMBER,
-                    SupplierId = r.SUPPLIER_GUID.Value,
-                    UserId = r.USER_GUID,
-                    Description = r.COMMENT,
-                    StartDate = r.STARTDATE,
-                    CompleteDate = r.ENDDATE,
-                    Client = new Client { Id = r.SUPPLIER_GUID.Value, Name = r.SUPPLIER.LOCATION_NAME },
-                    User = r.USER != null ? new User { Id = r.USER_GUID.Value, Name = r.USER.FIRST_NAME } : default(User)
-                });
-        }
-*/
-        public async Task<Setting> GetByCodeAsync(string code)
-        {
             _logger.LogInformation("Get setting by code {0}", code);
 
-            var response = await _client.GetAsync("/v1/kv/"+code);
+            /*
+         var response = await _client.GetAsync("/v1/kv/"+code);
 
-           _logger.LogInformation($"/v1/kv/{0}", code);
+        _logger.LogInformation($"/v1/kv/{0}", code);
 
-            if (!response.IsSuccessStatusCode)
-                throw new Exception(await response.Content.ReadAsStringAsync());
+         if (!response.IsSuccessStatusCode)
+             throw new Exception(await response.Content.ReadAsStringAsync());
 
-            /*    
-            var json = await response.Content.ReadAsStringAsync();
-            var setting = new Setting();
-            setting.Code = code;
-            setting.Content = json.Value; 
+         var json = await response.Content.ReadAsStringAsync();
+         var result = JsonConvert.DeserializeObject<Setting[]>(json).SingleOrDefault();
 
-            var result (JsonConvert.DeserializeObject<Setting>(json);
-            */
-            var json = await response.Content.ReadAsStringAsync();
-            var result = JsonConvert.DeserializeObject<Setting[]>(json).SingleOrDefault();
-			
+         return result;
 
-            return result;
         
-        }
 
-/*
-        public async Task<Job> CreateAsync(Guid ticketId, JobCreated created)
-        {
-            var job = new REQUEST_JOB {
-                REQUEST_GUID = ticketId,
-                SUPPLIER_GUID = created.SupplierId,
-                USER_GUID = created.UserId,
-                COMMENT = created.Description,
-                STARTDATE = DateTime.Now
-            };
 
-            await this.context.AddAsync(job);
-            await this.context.SaveChangesAsync();
-            
-            this.logger.LogInformation("Create new job");
-            
-            return await GetByIdAsync(job.GUID_RECORD);
-        }
-
-        public async Task DeleteAsync(Guid id)
-        {
-            var deleted = new REQUEST_JOB 
+            using (var client = new ConsulClient(_consuleconfiguration))
             {
-                GUID_RECORD = id
-            };
 
-            this.context.Remove(deleted);
-            await this.context.SaveChangesAsync();
+                var getPair = await client.KV.Get(code);
+
+                return new Setting
+                {
+                    Key = code,
+                    Value = Encoding.UTF8.GetString(getPair.Response.Value, 0, getPair.Response.Value.Length)
+                };
+            }
+
+             */
+            using (var client = new ConsulClient(_consuleconfiguration))
+            {
+
+                var getPair = await client.KV.Get(code);
+
+                return Encoding.UTF8.GetString(getPair.Response.Value, 0, getPair.Response.Value.Length);
+            }
+
         }
-          */
+
+        /*
+                public async Task<Job> CreateAsync(Guid ticketId, JobCreated created)
+                {
+                    var job = new REQUEST_JOB {
+                        REQUEST_GUID = ticketId,
+                        SUPPLIER_GUID = created.SupplierId,
+                        USER_GUID = created.UserId,
+                        COMMENT = created.Description,
+                        STARTDATE = DateTime.Now
+                    };
+
+                    await this.context.AddAsync(job);
+                    await this.context.SaveChangesAsync();
+
+                    this.logger.LogInformation("Create new job");
+
+                    return await GetByIdAsync(job.GUID_RECORD);
+                }
+
+                public async Task DeleteAsync(Guid id)
+                {
+                    var deleted = new REQUEST_JOB 
+                    {
+                        GUID_RECORD = id
+                    };
+
+                    this.context.Remove(deleted);
+                    await this.context.SaveChangesAsync();
+                }
+                  */
     }
 
   
